1eacd57b14ec22b43aa0a51e10384276888b3580
Brit <britcava@gmail.com>
2016-11-15 14:27:08 -0800

    Create sentiment_analysis_top_100_songs.py

---
+++ sentiment_analysis_top_100_songs.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import time
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+t0 = time.time()
+
+top_100 = pd.read_csv('/Users/brit.cava/Desktop/top100.csv')
+
+text = top_100['Word']
+scores = []
+words = []
+sid = SentimentIntensityAnalyzer()
+word_score_dict = {}
+
+for word in text:
+    words.append(word)
+    ss = sid.polarity_scores(word)
+    scores.append(ss['compound'])
+    
+
+for i in range(len(words)):
+    word_score_dict[words[i]] = scores[i]
+
+t1 = time.time()
+
+total = t1-t0
+
+print(total)
+    
+df = pd.DataFrame(list(word_score_dict.iteritems()), columns=['word','score'])
+
+df.to_csv("/Users/brit.cava/Desktop/Scores.csv", index=False, cols=('word','score'))
